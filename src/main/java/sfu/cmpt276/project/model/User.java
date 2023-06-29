61dd4113469abf2ed6f37b81910bcb8a4ac6df49
NickMoo5 <nicholasamah@gmail.com>
2023-06-29 13:21:06 -0700

    add more attributes to user model

--- src/main/java/sfu/cmpt276/project/model/User.java
+++ src/main/java/sfu/cmpt276/project/model/User.java
@@ -10,6 +10,9 @@ public class User {
     private int uid;
     private String username;
     private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
     private String accountType = ACCOUNTTYPE_USER;
 
     public static final String ACCOUNTTYPE_USER = "user";
@@ -44,5 +47,29 @@ public class User {
 
     public String getAccountType() {
         return accountType;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
