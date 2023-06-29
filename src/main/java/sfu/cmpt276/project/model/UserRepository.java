85ceef7cc0c837e5ba94da5de5d25dbfce0c9a11
NickMoo5 <nicholasamah@gmail.com>
2023-06-29 13:26:29 -0700

    add userrepo method to find by username

--- src/main/java/sfu/cmpt276/project/model/UserRepository.java
+++ src/main/java/sfu/cmpt276/project/model/UserRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 public interface UserRepository extends JpaRepository<User, Integer> {
     List<User> findByUserAndPass(String username, String password);
+    List<User> findByUser(String username);
 }
