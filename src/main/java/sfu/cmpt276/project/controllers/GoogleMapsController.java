f2945fde2dc1bb8981d2926f65d31aa155f8d6bc
hilokiyz <74848293+hilokiyz@users.noreply.github.com>
2023-07-25 11:14:00 -0700

    Implented new features using google maps api
    Moved origin city variable to MapsController, edit on line 12
    Added infowindow on restaurant markers, showing name, user rating, price level (if available), and address

--- src/main/java/sfu/cmpt276/project/controllers/GoogleMapsController.java
+++ src/main/java/sfu/cmpt276/project/controllers/GoogleMapsController.java
@@ -1,13 +1,16 @@
 package sfu.cmpt276.project.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GoogleMapsController {
 
     @GetMapping("/showMap")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("place", "Sydney Opera House");
         return "maps/index";
+
     }
 }
