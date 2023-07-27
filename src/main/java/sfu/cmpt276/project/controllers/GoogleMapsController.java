3539ee5bf5b9707c4c18821c97a0956748b26bef
hilokiyz <74848293+hilokiyz@users.noreply.github.com>
2023-07-27 09:01:45 -0700

    Begun merger
    Added map divs to each collapsible section and initialized each map accordingly

--- src/main/java/sfu/cmpt276/project/controllers/GoogleMapsController.java
+++ src/main/java/sfu/cmpt276/project/controllers/GoogleMapsController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GoogleMapsController {
 
+    //testing controller for google maps, do not use
     @GetMapping("/showMap")
     public String index(Model model) {
         model.addAttribute("place", "Sydney Opera House");
